03ab8dddea7753991ba5bf6628a0426e1f7bdcb1
onlymegithub <onlymegithub@gmail.com>
2016-09-09 00:42:54 -0400

    implimented devise

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # Required for Heroku
+  # note to set to actual host
+  config.action_mailer.default_url_options = { host: 'amonthr-pinteresting.herokuapp.com'}
 end
