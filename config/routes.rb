34f821e797920cf8637dbfa331a2691e749e561b
onlymegithub <onlymegithub@gmail.com>
2016-09-12 04:26:50 -0400

    update show view, add glyph icons and change rooth path

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   resources :pins
   devise_for :users
-  root 'pages#home'
+  root 'pins#index'
+  #root 'pages#home'
   get 'about' => 'pages#about'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
